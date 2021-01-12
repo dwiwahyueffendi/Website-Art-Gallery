077bb71179d4373ac2dea5032369770677a3b79e
dimasrehannabiel <dimasrehannabiel@gmail.com>
2021-01-13 02:17:54 +0700

    Front End Halaman Login dan Register Selesai

--- Code/login.php
+++ Code/login.php
@@ -40,23 +40,58 @@
 
 <!DOCTYPE html>
 <head>
-    <title>Login | Pamer.in</title>
+    <title>Login | ArtGallery</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/r-style.css">
     <script src="bootstrap/js/jquery.min.js"></script>
     <script src="bootstrap/js/js/bootstrap.min.js"></script>
-<body>
-    <?php if(isset($error)) : ?>
-        <p style="color:red"><b>Username / Password salah!</b></p>
-    <?php endif;?>
-    <form action="" method="post">
-        <label for="l_username">Username : </label><br>
-        <input type="username" name="l_username" id="" placeholder="Pick a username" required><br>
+<body style="background-color: #194E4A;">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 image-LogReg">
+               <h1 class="namaWebsite">ART GALLERY</h1>
+               <h2 class="sloganWebsite">
+                    Join<br>
+                    Largest<br>
+                    Art<br>
+                    Community<br>
+                    In The World<br>
+               </h2>
+               <p class="quoteWebsite">
+                    Explore and discover art, become a better artist,<br>
+                    connect with others over mutual hobbies, or<br>
+                    buy and sell work – you can do it all here.<br>
+                </p>
+            </div>
 
-        <label for="l_password">Password : </label><br>
-        <input type="password" name="l_password" id="" placeholder="Password" required><br>
+            <div class="col-lg-6 col-md-6 col-sm-12 side-LogReg">
+               <h1 class="LogReg">Log in</h1>
+               <p class="question-LogReg">Do you have an account?<a href="register.php" style="text-decoration: none; color: #FFF6C4"> Join</a></p>
 
-        <button type="submit" name="login">Login!</button>
-    </form>
+               <form action="" class="form-LogReg" method="post">
+                    <div class="form-group">
+                        <input type="username" name="l_username" class="form-control form-control-lg input-LogReg mb-3" placeholder="Username" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="l_password" class="form-control form-control-lg input-LogReg mb-3" placeholder="Password" required>
+                    </div>
+                    <button type="submit" name="login" class="btn btn-primary button-LogReg">LOG IN</button>
+                </form>
+
+                <?php if(isset($error)) : ?>
+                    <div class="alert alert-danger mt-3" role="alert" style="border-radius: 0px;">
+                        <p style="color:red"><b>Username / Password salah!</b></p>
+                    </div>
+                 <?php endif;?>
+
+                <p class="agreement-LogReg">
+                    By clicking Log In, I confirm that I have read and agree<br>
+                    to the ArtGallery Terms of Service, Privacy Policy, and<br>
+                    to receive emails and updates.<br>
+                </p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- Code/register.php
+++ Code/register.php
@@ -15,7 +15,7 @@
         if( registrasi($_POST) > 0 )
         {
             echo "<script>
-                    alert('User berhasil ditambahkan!');
+                    alert('Anda berhasil Mendaftar!');
                   </script>";
         }
         else
@@ -27,27 +27,59 @@
 
 <!DOCTYPE html>
 <head>
-    <title>Register | Pamer.in</title>
+    <title>Register | ArtGallery</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/r-style.css">
     <script src="bootstrap/js/jquery.min.js"></script>
     <script src="bootstrap/js/js/bootstrap.min.js"></script>
-<body>
-    <form action="" method="post">
-    <div>
-        <label for="r_username">Username : </label><br>
-        <input type="username" name="r_username" id="" placeholder="Pick a username" required><br>
+<body style="background-color: #194E4A;">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 image-LogReg">
+               <h1 class="namaWebsite">ART GALLERY</h1>
+               <h2 class="sloganWebsite">
+                    Join<br>
+                    Largest<br>
+                    Art<br>
+                    Community<br>
+                    In The World<br>
+               </h2>
+
+               <p class="quoteWebsite">
+                    Explore and discover art, become a better artist,<br>
+                    connect with others over mutual hobbies, or<br>
+                    buy and sell work – you can do it all here.<br>
+                </p>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-12 side-LogReg">
+               <h1 class="LogReg">Join</h1>
+               <p class="question-LogReg">Already have an account?<a href="login.php" style="text-decoration: none; color: #FFF6C4"> Log in</a></p>
+
+               <form action="" method="post" class="form-LogReg" style="margin-top: 30px;">
+                    <div class="form-group">
+                        <input type="username" name="r_username" class="form-control form-control-lg input-LogReg mb-3" placeholder="Pick a username">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" name="r_email" class="form-control form-control-lg input-LogReg mb-3" placeholder="Add your email">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="r_password" class="form-control form-control-lg input-LogReg mb-3" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="r_confirmPassword" class="form-control form-control-lg input-LogReg mb-3" placeholder="Confirm Password">
+                    </div>
+                    <button type="submit" name="register" class="btn btn-primary button-LogReg">JOIN</button>
+                </form>
+
+                <p class="agreement-LogReg">
+                    By clicking Log In, I confirm that I have read and agree<br>
+                    to the ArtGallery Terms of Service, Privacy Policy, and<br>
+                    to receive emails and updates.<br>
+                </p>
+            </div>
+        </div>
     </div>
-        <label for="r_email">Email : </label><br>
-        <input type="email" name="r_email" id="" placeholder="Add your email" required><br>
-
-        <label for="r_password">Password : </label><br>
-        <input type="password" name="r_password" id="" placeholder="Password" required><br>
-
-        <label for="r_confirmPassword">Confirm Password : </label><br>
-        <input type="password" name="r_confirmPassword" id="" placeholder="Confirm Password" required><br><br>
-
-        <button type="submit" name="register">Register!</button>
-    </form>
 </body>
 </html>
