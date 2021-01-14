63b6b7acdeb02e57fd22ae1f9b4739a380b78fb6
DwiWahyuEffendi <dwiwahyueffendi22@gmail.com>
2021-01-14 13:26:59 +0700

    ui focus

--- Code/sistem_load/load_komen.php
+++ Code/sistem_load/load_komen.php
@@ -18,32 +18,32 @@
                     </div>
                     <div class='col media-body'>
                         <div class='row'>
-                            <a style='color:black !important' href = 'userArt.php?username=". $row_komen['USERNAME'] ."'>
+                            <a style='color:black !important; margin-left:-20px;' href = 'userArt.php?username=". $row_komen['USERNAME'] ."'>
                                 <h6 class='col-2 media-heading'>
                                     <b>". $row_komen['USERNAME'] ."</b>
                                 </h6>
                             </a>
                             <div class='col-1 media-body'>
-                                <p style='font-size:12px'>  ". $row_komen['TANGGALKOMENTAR'] ."</p>
+                                <p style='font-size:12px; margin-left:-20px;'>  ". $row_komen['TANGGALKOMENTAR'] ."</p>
                             </div>  
                         </div>
                         <div class='media-body justify'>
-                            <form id='e_form". $row_komen['IDKOMENTAR'] ."' style='display:none' method='POST'>
-                                <textarea id='e_area". $row_komen['IDKOMENTAR'] ."' name='e_komen' class='form-control'  rows='4' placeholder='". $row_komen['ISIKOMENTAR'] ."'></textarea>
+                            <form id='e_form". $row_komen['IDKOMENTAR'] ."' style='display:none;' method='POST'>
+                                <textarea id='e_area". $row_komen['IDKOMENTAR'] ."' name='e_komen' class='form-control'  rows='4' style='margin-left:-20px;'placeholder='". $row_komen['ISIKOMENTAR'] ."'></textarea>
                                 <input type='hidden' value=". $row_komen['IDKOMENTAR'] ." name='id_comment'>
                                 <br>
                                 <div class='right'>
                                     <button type='submit' class='btn btn-primary fBaloo base-color' id='edit_it". $row_komen['IDKOMENTAR'] ."' name='edit_it' >Edit it!</button>
                                 </div>
                             </form>
-                            <p id='par_komen". $row_komen['IDKOMENTAR'] ."'>". $row_komen['ISIKOMENTAR'] ."</p> 
+                            <p id='par_komen". $row_komen['IDKOMENTAR'] ."' style='margin-left:-20px'>". $row_komen['ISIKOMENTAR'] ."</p> 
                             <div class='row'>
                                 <div class='col-2' >
-                                    <button type='submit' style='font-size:12px;background-color:transparent' class='btn' id='open_edit". $row_komen['IDKOMENTAR'] ."' name='edit_it'>Edit</button>
+                                    <button type='submit' class='btn btn-primary fBaloo base-color' id='open_edit". $row_komen['IDKOMENTAR'] ."' name='edit_it' style='margin-left:-20px'>Edit</button>
                                 </div>
                                 <form class='col-1' method='POST'>
                                     <input type='hidden' value=". $row_komen['IDKOMENTAR'] ." name='id_comment'>
-                                    <button type='submit' style='font-size:12px;background-color:transparent' class='btn' id='delete_it". $row_komen['IDKOMENTAR'] ."' name='delete_it'>Delete</button>
+                                    <button type='submit' class='btn btn-primary fBaloo base-color' id='delete_it". $row_komen['IDKOMENTAR'] ."' name='delete_it'>Delete</button>
                                 </form>
                             </div>
 
